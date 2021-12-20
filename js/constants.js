93197bebe4cfcd4c9def6efe2ea93d58ff463f7f
Carlos Fernando <ca_fer89@hotmail.com>
2021-12-19 21:10:02 -0500

    second commit

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
 export const API_KEY = 'b958decc59e5b9a0497bb7dc8d3b04f8'
-export const BASE_API = 'http://api.openweathermap.org/data/2.5/'
+export const BASE_API = 'https://api.openweathermap.org/data/2.5/'
 
 
 export const weatherConditionCodes = {
---
+++ utils/viewport.js
@@ -0,0 +1,24 @@
+
+export function setViewportSize($el) {
+  const viewportBlockSize = getViewport()
+  $el.style.blockSize = `${viewportBlockSize}px`
+}
+
+export function getViewport() {
+  return window.innerHeight
+}
+
+export function onViewportResize(callback) {
+  window.addEventListener('resize', callback)
+}
+
+export function offViewportResize () {
+  window.removeEventListener('resize', callback)
+}
+
+export function viewportSize($el) {
+  setViewportSize($el)
+
+  onViewportResize(() => setViewportSize($el))
+
+}
